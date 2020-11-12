e24514ba5e7b0de0b7bc474d909d88bafa09268e
Максим <maxpoint2point@gmai.com>
2020-11-12 15:31:04 +0300

    fix

--- PhoneBook/webui/urls.py
+++ PhoneBook/webui/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.PhonesView.as_view()),
+    path("", views.PhonesView.as_view(), name="main"),
     path("add", views.AddPhone.as_view(), name="add_phone"),
     path("search/", views.Search.as_view(), name="search"),
     path('phone/<int:pk>/', views.PhoneDetail.as_view(), name="detail"),
